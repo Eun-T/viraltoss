5b9cc04b1ae444eac4faa23d075dde6c0a791d63
Euntae <venzire329@gmail.com>
2023-10-15 00:57:19 +0900

    Feat: height fixed

--- components/Common/Main.tsx
+++ components/Common/Main.tsx
@@ -7,11 +7,18 @@ import { helvetica, jalnan } from "@/app/layout";
 type Props = {};
 
 const Main = (props: Props) => {
-  let vh = 0;
+
+  function setScreenHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 
   useEffect(() => {
-    vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    setScreenHeight();
+    
+    // resize 이벤트가 발생하면 다시 계산하도록 아래 코드 추가
+    window.addEventListener('resize', setScreenHeight);
+    return () => window.removeEventListener('resize', setScreenHeight);
   }, []);
 
   const isArrowDown = () => {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,54 +1,59 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
-      's': '480px', 
-      'm': '768px', 
-      'l': '1024px', 
-      },
+      s: "480px",
+      m: "768px",
+      l: "1024px",
+    },
+    minHeight: {
+      "real-screen": "calc(var(--vh) * 100)",
+    },
     extend: {
-
+      height: {
+        "real-screen": "calc(var(--vh) * 100)",
+      },
       keyframes: {
         textShow: {
-          '0%': { backgroundColor: "transparent" },
-          '100%': { backgroundColor: "#4e5968" }
+          "0%": { backgroundColor: "transparent" },
+          "100%": { backgroundColor: "#4e5968" },
         },
         buttonShow: {
-          '0%': { transform: "translateY(0)" },
-          '20%': { transform: "translateY(-10px)" },
-          '40%': { transform: "translateY(0)" },
-          '70%': { transform: "translateY(-20px)"},
-          '100%': { transform: "translateY(0)" }
+          "0%": { transform: "translateY(0)" },
+          "20%": { transform: "translateY(-10px)" },
+          "40%": { transform: "translateY(0)" },
+          "70%": { transform: "translateY(-20px)" },
+          "100%": { transform: "translateY(0)" },
         },
         imageUp: {
-          '0%': { transform: "translateY(0px)" },
-          '100%': { transform: "translateY(-20px)" }
+          "0%": { transform: "translateY(0px)" },
+          "100%": { transform: "translateY(-20px)" },
         },
         imageDown: {
-          '0%': { transform: "translateY(-20px)" },
-          '100%': { transform: "translateY(0px)" }
+          "0%": { transform: "translateY(-20px)" },
+          "100%": { transform: "translateY(0px)" },
         },
         barUp: {
-          '0%': { opacity: ".3" },
-          '50%': { opacity: "1" },
-          '100%': { opacity: ".3" }
+          "0%": { opacity: ".3" },
+          "50%": { opacity: "1" },
+          "100%": { opacity: ".3" },
         },
       },
       animation: {
-        textShow: 'textShow .5s ease',
-        buttonShow: 'buttonShow 2s ease',
-        imageUp: 'imageUp .5s ease',
-        imageDown: 'imageDown .5s ease',
-        barUp: 'barUp .5s ease',
+        textShow: "textShow .5s ease",
+        buttonShow: "buttonShow 2s ease",
+        imageUp: "imageUp .5s ease",
+        imageDown: "imageDown .5s ease",
+        barUp: "barUp .5s ease",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
