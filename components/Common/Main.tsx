127f67656f388cbbe6eff4b998df793dbe08d93e
Euntae <venzire329@gmail.com>
2023-10-15 01:21:00 +0900

    Feat: ios fixed

--- components/Common/Main.tsx
+++ components/Common/Main.tsx
@@ -8,19 +8,6 @@ type Props = {};
 
 const Main = (props: Props) => {
 
-  function setScreenHeight() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }
-
-  useEffect(() => {
-    setScreenHeight();
-    
-    // resize 이벤트가 발생하면 다시 계산하도록 아래 코드 추가
-    window.addEventListener('resize', setScreenHeight);
-    return () => window.removeEventListener('resize', setScreenHeight);
-  }, []);
-
   const isArrowDown = () => {
     window.scrollTo({ top: 780, behavior: "smooth" });
   };
